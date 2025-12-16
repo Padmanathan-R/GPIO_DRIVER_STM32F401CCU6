a15dea539e99ad49b6220cbb205aef6654e99ce3
Padmanathan R <140795998+Padmanathan-R@users.noreply.github.com>
2025-12-16 23:15:54 +0530

    Add files via upload
    A bare-metal GPIO driver for the STM32F401CCU6 (ARM Cortex-M4) microcontroller, written entirely from scratch in C. This project demonstrates register-level programming and provides low-level control of the GPIO peripheral without using HAL or CubeMX.
    
    Features:
     1)Enable clocks for GPIO ports (A, B, C, E, H)
     2)Configure GPIO pins as input, output, analog, or alternate function
     3)Set pull-up/pull-down/no pull configuration for input pins
     4)Read pin state using the Input Data Register (IDR)
     5)Write output values using the Output Data Register (ODR).
    
     Fully bare-metal, lightweight, and easy to understand

---
+++ main.h
@@ -0,0 +1,114 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 16, 2025
+ *      Author: Padmanathan R
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdint.h>
+
+typedef struct
+{
+	uint32_t gpioa_en  : 1;
+	uint32_t gpiob_en  : 1;
+	uint32_t gpioc_en  : 1;
+	uint32_t gpioe_en  : 1;
+	uint32_t reserved1 : 2;
+	uint32_t gpioh_en  : 1;
+	uint32_t reserved2 : 4;
+	uint32_t crc_en    : 1;
+	uint32_t reserved3 : 3;
+	uint32_t reserved4 : 5;
+	uint32_t dma1_en   : 1;
+	uint32_t dma2_en   : 1;
+	uint32_t reserved5 : 9;
+}RCC_AHB1ENR_t;
+
+typedef struct
+{
+	uint32_t moder_0   : 2;
+	uint32_t moder_1   : 2;
+	uint32_t moder_2   : 2;
+	uint32_t moder_3   : 2;
+	uint32_t moder_4   : 2;
+	uint32_t moder_5   : 2;
+	uint32_t moder_6   : 2;
+	uint32_t moder_7   : 2;
+	uint32_t moder_8   : 2;
+	uint32_t moder_9   : 2;
+	uint32_t moder_10  : 2;
+	uint32_t moder_11  : 2;
+	uint32_t moder_12  : 2;
+	uint32_t moder_13  : 2;
+	uint32_t moder_14  : 2;
+	uint32_t moder_15  : 2;
+}GPIOx_MODER_t;
+
+typedef struct
+{
+	uint32_t odr_0     : 1;
+	uint32_t odr_1     : 1;
+	uint32_t odr_2     : 1;
+	uint32_t odr_3     : 1;
+	uint32_t odr_4     : 1;
+	uint32_t odr_5     : 1;
+	uint32_t odr_6     : 1;
+	uint32_t odr_7     : 1;
+	uint32_t odr_8     : 1;
+	uint32_t odr_9     : 1;
+	uint32_t odr_10    : 1;
+	uint32_t odr_11    : 1;
+	uint32_t odr_12    : 1;
+	uint32_t odr_13    : 1;
+	uint32_t odr_14    : 1;
+	uint32_t odr_15    : 1;
+	uint32_t reserved  : 16;
+}GPIOx_ODR_t;
+
+typedef struct
+{
+	uint32_t pupdr_0   : 2;
+	uint32_t pupdr_1   : 2;
+	uint32_t pupdr_2   : 2;
+	uint32_t pupdr_3   : 2;
+	uint32_t pupdr_4   : 2;
+	uint32_t pupdr_5   : 2;
+	uint32_t pupdr_6   : 2;
+	uint32_t pupdr_7   : 2;
+	uint32_t pupdr_8   : 2;
+	uint32_t pupdr_9   : 2;
+	uint32_t pupdr_10  : 2;
+	uint32_t pupdr_11  : 2;
+	uint32_t pupdr_12  : 2;
+	uint32_t pupdr_13  : 2;
+	uint32_t pupdr_14  : 2;
+	uint32_t pupdr_15  : 2;
+}GPIOx_PUPDR_t;
+
+typedef struct
+{
+	uint32_t idr_0     : 1;
+	uint32_t idr_1     : 1;
+	uint32_t idr_2     : 1;
+	uint32_t idr_3     : 1;
+	uint32_t idr_4     : 1;
+	uint32_t idr_5     : 1;
+	uint32_t idr_6     : 1;
+	uint32_t idr_7     : 1;
+	uint32_t idr_8     : 1;
+	uint32_t idr_9     : 1;
+	uint32_t idr_10    : 1;
+	uint32_t idr_11    : 1;
+	uint32_t idr_12    : 1;
+	uint32_t idr_13    : 1;
+	uint32_t idr_14    : 1;
+	uint32_t idr_15    : 1;
+	uint32_t reserved  : 16;
+}GPIOx_IDR_t;
+
+
+
+
+#endif /* MAIN_H_ */
